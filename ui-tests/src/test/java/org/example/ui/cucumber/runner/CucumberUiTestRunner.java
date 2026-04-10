3f965f394ea32fd8a4d9b4f1ffd0b346e8827909
VitaliTalipski <vitalitalipski@gmail.com>
2026-04-10 14:27:18 +0300

    Allure ConfigurationParameter

--- ui-tests/src/test/java/org/example/ui/cucumber/runner/CucumberUiTestRunner.java
+++ ui-tests/src/test/java/org/example/ui/cucumber/runner/CucumberUiTestRunner.java
@@ -5,9 +5,15 @@ import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.example.ui.cucumber")
+@ConfigurationParameter(
+        key = PLUGIN_PROPERTY_NAME,
+        value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+)
+
 public class CucumberUiTestRunner {
 }
